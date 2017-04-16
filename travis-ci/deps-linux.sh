c39db5f030c7295c1bfaee71ed4bc0787fb0f179
beckrd <beck@ripabe.net>
2017-04-16 17:04:55 -0500

    Update deps-linux.sh

--- travis-ci/deps-linux.sh
+++ travis-ci/deps-linux.sh
@@ -3,9 +3,9 @@
 set -x
 
 sudo apt-get -qq update
-sudo apt-get -q -y install libusb-1.0-0-dev libcfitsio3-dev libnova-dev
+sudo apt-get -q -y install libusb-1.0-0-dev libcfitsio-dev libnova-dev
 sudo apt-get -q -y install libgphoto2-dev libgps-dev libjpeg-dev cdbs
-sudo apt-get -q -y install libopenal-dev libgsl0-dev libboost-dev
+sudo apt-get -q -y install libopenal-dev libgsl-dev libboost-dev
 sudo apt-get -q -y install libboost-regex-dev libftdi-dev dcraw fakeroot
 sudo apt-get -q -y install wget curl
 
